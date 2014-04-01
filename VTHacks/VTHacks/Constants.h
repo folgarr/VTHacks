44097b29dd3d703eed18bcb0f84ef92c1e473853
Carlos Folgar <folgar.tech@gmail.com>
2014-04-01 12:15:00 -0400

    small change

--- VTHacks/VTHacks/Constants.h
+++ VTHacks/VTHacks/Constants.h
@@ -1,7 +1,5 @@
 
-#define ACCESS_KEY_ID                @"ASIAIVBCPPY7Z52DZ6AA"
-#define SECRET_KEY                   @"IizwIQMI6I3/bt8kPbKecSkbqOOB+GueUi6oxOSM"
-#define SECURITY_TOKEN               @"AQoDYXdzEMf//////////wEaoAKf7sgYM5BOTxChLmqMBdJbxnFx6X8TmMx7DtwLhroJuUQcgobB648U+0yNc9lG5u8nG/6UAVOo+WdpR+oRJa5x90aT2NKcIgw3ISKIgRgJJrhwfYzHfTVkG/i5TTPFAquaE+zhkv3Jibj1JNZm4QsS3CioVGNeTT5FOGndBFERFypvMMf6HyC/cY/P1yf400zHKlE0ZEtdTEKJsMCOf0JFD0dlKhvfSS5qcyqITeXgV2SKRiP3XOXjpBTnAjv0rb6463gvy/4SDNvZNDdf3GF4NSceUmu+YW49FeLnHPbcFJUb/lje6lKaeuJL2j/v8ktqKdbtcQPaK5mxftHcyLa3V2fMhsYUaStG9xDoGtYHHxsbdzZmPHh2gtbDGJuvhfAgl6LimQU="
+
 #define CONFIRM_SUBSCRIPTION_MESSAGE    @"A confirmation must be accepted before messages are received."
 #define QUEUE_NOTICE                    @"It may take a few minutes before the queue starts receiving messages."
 #define SMS_SUBSCRIPTION_MESSAGE        @"SMS Subscritions must include country codes.  1 for US phones."
